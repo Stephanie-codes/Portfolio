1b03c712d3b79ffa5ff4789abb49cbdaa7926297
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-04-05 19:26:30 +0000

    contact form using nodemailer

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-
 const text = "Hello, World!";
 let index = 0;
 
@@ -10,19 +9,54 @@ function type() {
 
 setInterval(type, 250); // adjust the interval time to change typing speed
 
-
-
 window.onscroll = function() {
     var navigation = document.getElementById("navigation");
     var main = document.getElementById("main");
 
     if (window.pageYOffset >= navigation.offsetTop) {
         navigation.classList.add("sticky");
-        navigation.classList.add("opaque"); // add class to make nav bar opaque
+        navigation.classList.add("opaque");
         main.classList.add("content-sticky");
     } else {
         navigation.classList.remove("sticky");
-        navigation.classList.remove("opaque"); // remove class to make nav bar transparent again
+        navigation.classList.remove("opaque");
         main.classList.remove("content-sticky");
     }
 };
+
+//Contact form
+const contactForm = document.querySelector('.contact-form');
+
+let fullName = document.getElementById('fullname');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    let formInput = {
+        fullName: fullName.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText === 'success'){
+            alert('Email sent');
+            fullName.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!');
+        }
+    }
+
+    xhr.send(JSON.stringify(formInput));
+});
--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const app = express();
 
+const nodemailer = require('nodemailer');
+
 const PORT = process.env.PORT || 8080;
 
 // Middleware 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const client = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "stephaniecodes202@gmail.com",
+            pass: ""
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: "stephaniecodes202@gmail.com",
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    client.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+        }
+    })
 });
 
 app.listen(PORT, () => {
